fb485624cc48e0ba6687f66f6a478c3a9ddcb07c
wyl <1052063507@qq.com>
2025-02-08 17:10:13 +0800

    test

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import useVxeTable from './vxe-table'
-
-createApp(App).use(useVxeTable).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,64 @@
+import { URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {lazyImport, VxeResolver} from 'vite-plugin-lazy-import'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import viewport from 'postcss-mobile-forever'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        lazyImport({
-            resolvers: [
-                VxeResolver({
-                    libraryName: 'vxe-table'
+    root: './src/packages/',
+    // root: resolve(__filename, 'src/packages/'),
+    plugins: [vue(), vueJsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        open: true, // 是否自动在浏览器打开
+    },
+    css: {
+        postcss: {
+            plugins: [
+                // https://github.com/wswmsword/postcss-mobile-forever
+                viewport({
+                    appSelector: '#mobile',
+                    viewportWidth: 375,
+                    maxDisplayWidth: 600,
+                    rootContainingBlockSelectorList: [
+                        'van-tabbar',
+                        'van-popup',
+                    ],
+                    exclude: [/src\/packages\/index/],
+                    border: true,
                 }),
-                VxeResolver({
-                    libraryName: 'vxe-pc-ui'
-                })
-            ]
-        })],
+            ],
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                // http://127.0.0.1:5173
+                main: resolve(__dirname, 'src/packages/index.html'),
+                // http://127.0.0.1:5173/page1.html
+                mobile: resolve(__dirname, 'src/packages/mobile.html'),
+            },
+            /* input: {
+               app1: resolve(__dirname, 'src/packages/index.html'),
+               app2: resolve(__dirname, 'src/packages/app2.html'),
+             },*/
+            // input: [resolve(__dirname, 'src/packages/index.html'),  resolve(__dirname, 'src/packages/app2.html')],
+            // input: ['./src/packages/index.html', './src/packages/page1.html'],
+            output: {
+                dir: './dist',
+                // chunkFileNames: 'js/[name]-[hash].js',
+                // entryFileNames: 'js/[name]-[hash].js',
+                // assetFileNames: '[ext]/[name]-[hash].[ext]',
+            }
+        }
+    }
 })
